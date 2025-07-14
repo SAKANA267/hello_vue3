bce341449573028179f25ef2ad744df6f000ff20
SAKANA'PC <ibert1ne@outlook.com>
2025-07-15 01:27:18 +0800

    create vue3 project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- html -->
+     <div class="app">
+        <h1>Hello Vue3</h1>
+        <Test />
+     </div>
+</template>
+
+<script lang="ts">
+    //js or ts
+    import Test from './components/Test.vue';
+    export default {
+        name: 'App',
+        components: {
+            Test
+        },
+    }
+</script>
+
+<style>
+    /* css */
+    .app{
+        background-color: #ddd;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        border-radius: 8px;
+    }
+</style>
